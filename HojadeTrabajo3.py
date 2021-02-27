21d76e343e7297e2c282bda1cda3f3cdccaa0896
Christian <chrisaragon4@gmail.com>
2021-02-26 23:28:58 -0600

    Hoja de trabajo 3

---
+++ HojadeTrabajo3.py
@@ -0,0 +1,26 @@
+#Ejercicio 1
+
+contraseña = str(input("Ingrese una contraseña: "))
+
+contraseña2 = str(input("Ingrese nuevamente su contraseña: "))
+
+if contraseña == contraseña2:
+    print("La contraseña es correcta!")
+else: 
+    print("La contraseña no coincide :(")
+
+
+#Ejercicio 2
+
+nombre = str(input("¿Cuál es su nombre?: "))
+
+sexo = str(input("¿Cuál es su sexo? (ingrese 'm' o 'f'):"))
+
+for i in range('a', 'm'):
+    i == 0
+    break
+
+if i == 0 and sexo == 'm':
+    print("Usted pertenece al grupo A")
+else: 
+    print("Usted pertenece al grupo B")
